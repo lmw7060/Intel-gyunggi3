1039cab5cc1dd599c104b253636ba45a9a1f6688
hulklee1 <hulklee1@gmail.com>
2023-12-18 11:59:56 +0900

    1

--- Test04-UART/Core/Inc/stm32f4xx_it.h
+++ Test04-UART/Core/Inc/stm32f4xx_it.h
@@ -58,6 +58,7 @@ void SysTick_Handler(void);
 void DMA1_Stream5_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
